cad8cc6a6db9d922a9cb6d2cdec01e93cf06a6a8
Ian Greulich <ian@greulich.me>
2015-10-05 15:15:16 -0400

    update nav for books

--- app/scripts/components/Nav.jsx
+++ app/scripts/components/Nav.jsx
@@ -11,8 +11,8 @@ module.exports = React.createClass({
 
   render() {
     var authLink;
-		var liStyles = {
-			visibility: 'hidden'
+    var liStyles = {
+      visibility: 'hidden'
     }
 
     if (this.props.user) {
@@ -21,44 +21,44 @@ module.exports = React.createClass({
       authLink = <li><a href="#" onClick={this.login}>Sign In</a></li>
     }
 
-		return (
-			<header className="header">
-				<Grid>
-					<Row>
-						<Col md={6}>
-							<div className="logo-white">
-								<img src="images/logo-white.png" />
-								<h3>GAMES</h3>
-							</div>
-						</Col>
-						<Col md={6} className="nav-wrapper">
-							<nav className="navbar navbar-inverse navbar-static-top" role="navigation">
-								<div className="navbar-header">
-									<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-										<span className="sr-only">Toggle navigation</span>
-										<span className="icon-bar"></span>
-										<span className="icon-bar"></span>
-										<span className="icon-bar"></span>
-									</button>
-								</div>
-								<div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-									<ul className="nav navbar-nav">
-										<li style={liStyles}><a href="#">Profile</a></li>
+    return (
+      <header className="header">
+        <Grid>
+          <Row>
+            <Col md={6}>
+              <div className="logo-white">
+                <img src="images/logo-white.png" />
+                <h3>BOOKS</h3>
+              </div>
+            </Col>
+            <Col md={6} className="nav-wrapper">
+              <nav className="navbar navbar-inverse navbar-static-top" role="navigation">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span className="sr-only">Toggle navigation</span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                </div>
+                <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                  <ul className="nav navbar-nav">
+                    <li style={liStyles}><a href="#">Profile</a></li>
                     {authLink}
                   </ul>
-									<form>
-										<input type="text" name="search" ref="search" placeholder="Search" value={this.props.query} onChange={this.search} />
+                  <form>
+                    <input type="text" name="search" ref="search" placeholder="Search" value={this.props.query} onChange={this.search} />
 
-										<input type="image" src="images/icon-search.png" alt="Submit" />
-									</form>
-								</div>
-							</nav>
-						</Col>
-					</Row>
-				</Grid>
-			</header>
-		);
-	},
+                    <input type="image" src="images/icon-search.png" alt="Submit" />
+                  </form>
+                </div>
+              </nav>
+            </Col>
+          </Row>
+        </Grid>
+      </header>
+    );
+  },
 
   login() {
     this.props.onLogin();
