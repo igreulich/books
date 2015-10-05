ad47e4771106b9f4be66e23ab070a267c232cf56
Ian Greulich <ian@greulich.me>
2015-10-05 15:05:36 -0400

    update list component for books

--- app/scripts/components/BookList.jsx
+++ app/scripts/components/BookList.jsx
@@ -1,75 +1,52 @@
-'use strict'
+'use strict';
 
-var GameItem      = require('./GameItem');
+var BookItem      = require('./BookItem');
 var EditItem      = require('./EditItemForm');
-var SelectedModal = require('./Modal');
 const {
   Grid,
-  Row,
-  Button,
-  ModalTrigger
+  Row
 } = require('react-bootstrap');
 
 module.exports = React.createClass({
-  displayName: 'GameList',
+  displayName: 'BookList',
 
   getInitialState() {
     return {
-      selectedGame: ''
+      selectedBook: ''
     };
   },
 
   render() {
-    var gameAction = game => {
-      if (game.isEditing) {
-        return <EditItem key={game.id} game={game} onUpdate={this.props.onUpdate} />;
+    var bookAction = book => {
+      if (book.isEditing) {
+        return <EditItem key={book.id} book={book} onUpdate={this.props.onUpdate} />;
       } else {
-        return <GameItem key={game.id} game={game} user={this.props.user} onEdit={() => this.props.onEdit(game)} onDestroy={() => this.props.onDestroy(game)} />;
+        return <BookItem key={book.id} book={book} user={this.props.user} onEdit={() => this.props.onEdit(book)} onDestroy={() => this.props.onDestroy(book)} />;
       }
-		};
+    };
 
     return (
-      <section className="players">
+      <section className='players'>
         <Grid>
-          <div className="email-wrapper">
-            <h3 className="pull-left">Greulich's Games</h3>
-            <h4 className="pull-right">Total: {this.props.games.length}</h4>
-            <div className="clearfix"></div>
+          <div className='email-wrapper'>
+            <h3 className='pull-left'>Greulich's Books</h3>
+            <h4 className='pull-right'>Total: {this.props.books.length}</h4>
+            <div className='clearfix'></div>
           </div>
-          <Row>
-            <ModalTrigger modal={<SelectedModal game={this.state.selectedGame} />}>
-              <Button className="pull-right btn-default" type="button" onClick={() => this.choose()}>Choose a game</Button>
-            </ModalTrigger>
-          </Row>
-          <Row className="table-wrapper">
-            <table className="table table-striped">
+          <Row className='table-wrapper'>
+            <table className='table table-striped'>
               <thead>
                 <tr>
-                  <th className="table-name">Game <span className="pull-right"><a href="#" onClick={this.props.onAsecSort}><span className="glyphicon glyphicon-chevron-up"></span></a><a href="#" onClick={this.props.onDescSort}><span className="glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a></span></th>
-                  <th className="table-position">Players</th>
-                  <th className="table-points">Co-op</th>
-                  <th className="table-points">Type</th>
+                  <th className='table-name'>Book <span className='pull-right'><a href='#' onClick={this.props.onAsecSort}><span className='glyphicon glyphicon-chevron-up'></span></a><a href='#' onClick={this.props.onDescSort}><span className='glyphicon glyphicon-chevron-down' aria-hidden='true'></span></a></span></th>
                 </tr>
               </thead>
               <tbody>
-                {this.props.games.map(gameAction)}
+                {this.props.books.map(bookAction)}
               </tbody>
             </table>
           </Row>
         </Grid>
       </section>
     );
-	},
-
-  choose() {
-    var games = this.props.games.filter(element => {
-      return element.expansion === 'Base';
-    });
-
-    var key = Math.floor(Math.random() * games.length);
-
-    this.setState({
-      selectedGame: games[key]
-    });
   }
 });
