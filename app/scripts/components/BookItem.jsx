1948aafaef21a8ebc380bc29cc297c9582bfc274
Ian Greulich <ian@greulich.me>
2015-10-05 15:08:32 -0400

    update item component for books

---
+++ app/scripts/components/BookItem.jsx
@@ -0,0 +1,29 @@
+'use strict'
+
+module.exports = React.createClass({
+  displayName: 'BookItem',
+
+  render() {
+    var spanStyles = {
+      display: this.props.user ? '' : 'none'
+    };
+
+    return (
+      <tr>
+        <td>{this.props.book.name} <span className="pull-right" style={spanStyles}><a href='#' onClick={this.edit}>edit</a> | <a href='#' onClick={this.destroy}>delete</a></span></td>
+      </tr>
+    );
+  },
+
+  edit(event) {
+    event.preventDefault();
+
+    this.props.onEdit();
+  },
+
+  destroy(event) {
+    event.preventDefault();
+
+    this.props.onDestroy();
+  }
+});
