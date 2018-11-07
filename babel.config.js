248636d1929b3e9389f185d61ee8bbed6e4b14e5
Ian Greulich <ian@greulich.me>
2018-11-06 23:31:14 -0500

    linter fixes

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,10 @@
-module.exports = api => {
+module.exports = (api) => {
   const presets = [
     ['@babel/preset-env', { useBuiltIns: 'entry' }],
     '@babel/preset-react',
     'airbnb',
   ];
-  const plugins = [
-    'react-hot-loader/babel',
-    '@babel/plugin-proposal-class-properties',
-  ];
+  const plugins = ['react-hot-loader/babel', '@babel/plugin-proposal-class-properties'];
 
   api.cache(true);
 
