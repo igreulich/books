2e8e8a597cdb41787aa7702bb35655ffc1ec0013
Ian Greulich <ian@greulich.me>
2018-11-06 23:26:25 -0500

    update babel config

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = api => {
+  const presets = [
+    ['@babel/preset-env', { useBuiltIns: 'entry' }],
+    '@babel/preset-react',
+    'airbnb',
+  ];
+  const plugins = [
+    'react-hot-loader/babel',
+    '@babel/plugin-proposal-class-properties',
+  ];
+
+  api.cache(true);
+
+  return {
+    presets,
+    plugins,
+  };
+};
