c00b7b022b11c44da061f433a9cd90d18a3b7bdb
Ian Greulich <ian@greulich.me>
2018-11-17 23:15:10 -0500

    initial layout, wire up css

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
 
+import Header from './components/Header';
+
 import '@babel/polyfill';
 import 'airbnb-browser-shims';
 
 import '../styles/app.less';
 
-const App = () => <div>Hello World</div>;
+const App = () => <Header />;
 
 export default hot(module)(App);
