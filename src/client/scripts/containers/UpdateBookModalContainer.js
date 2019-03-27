17040425960a7f18c09b74a05fbe064c101ef806
Ian Greulich <ian@greulich.me>
2019-03-27 16:34:06 -0400

    update existing modal containers to account for base component changes

--- src/client/scripts/containers/UpdateBookModalContainer.js
+++ src/client/scripts/containers/UpdateBookModalContainer.js
@@ -2,14 +2,14 @@ import { connect } from 'react-redux';
 
 import { updateBook } from '../reducers/books';
 
-import UpdateBookModalContainer from '../components/modals/UpdateBookModal';
+import UpdateBookModalContainer from '../components/modals/UpsertBookModal';
 
 const mapStateToProps = state => ({
   book: state.books.book,
 });
 
 const mapDispatchToProps = dispatch => ({
-  handleUpdateBook: (id, title, series, number) => dispatch(updateBook(id, title, series, number)), // eslint-disable-line object-curly-newline
+  handleUpsertBook: (id, title, series, number) => dispatch(updateBook(id, title, series, number)), // eslint-disable-line object-curly-newline
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(UpdateBookModalContainer);
