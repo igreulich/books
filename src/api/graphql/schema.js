ab42b191a5d71a8f05df7090910305a1a6849f6b
Ian Greulich <ian@greulich.me>
2019-04-01 11:39:07 -0400

    mutations apparently need to be declared async, because reasons

--- src/api/graphql/schema.js
+++ src/api/graphql/schema.js
@@ -16,7 +16,7 @@ const typeDefs = gql`
   }
 
   type Mutation {
-    createBook(title: String!, series: String): Book!
+    createBook(title: String!, series: String, number: Int): Book!
     updateBook(id: ID!, title: String, series: String, number: Int): Book!
     deleteBook(id: ID!): Int!
   }
@@ -28,13 +28,9 @@ const resolvers = {
     book: (_, id) => Book.query().where(id).then(res => res[0]),
   },
   Mutation: {
-    updateBook: (_, { id, title, series, number }) => ( // eslint-disable-line object-curly-newline
-      Book.query().updateAndFetchById(id, { title, series, number })
-    ),
-    createBook: (_, { title, series, number }) => (
-      Book.query().insert({ title, series, number })
-    ),
-    deleteBook: (_, { id }) => Book.query().where({ id }).delete(),
+    updateBook: async (_, { id, title, series, number }) => Book.query().updateAndFetchById(id, { title, series, number }), // eslint-disable-line object-curly-newline
+    createBook: async (_, { title, series, number }) => Book.query().insert({ title, series, number }),
+    deleteBook: async (_, { id }) => Book.query().where({ id }).delete(),
   },
 };
 
