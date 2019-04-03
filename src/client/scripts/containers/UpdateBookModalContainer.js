2389bdb5744c6bb2d2a57c2f273e71f636e45ee0
Ian Greulich <ian@greulich.me>
2019-04-03 12:04:08 -0400

    you are ont importing a conatiner, you are importing a component

--- src/client/scripts/containers/UpdateBookModalContainer.js
+++ src/client/scripts/containers/UpdateBookModalContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 
 import { updateBook } from '../reducers/books';
 
-import UpsertBookModalContainer from '../components/modals/UpsertBookModal';
+import UpsertBookModal from '../components/modals/UpsertBookModal';
 
 const mapStateToProps = state => ({
   book: state.books.book,
@@ -12,4 +12,4 @@ const mapDispatchToProps = dispatch => ({
   handleUpsertBook: (id, title, series, number) => dispatch(updateBook(id, title, series, number)), // eslint-disable-line object-curly-newline
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(UpsertBookModalContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(UpsertBookModal);
